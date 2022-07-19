8563d6587b3c13616b13425e66464fe351745972
haythembensouissi <haythembensouissi2@gmail.com>
2022-07-19 17:45:16 +0100

    working on the diets part

--- better B/client/app/pages/Diet.tsx
+++ better B/client/app/pages/Diet.tsx
@@ -1,8 +1,33 @@
-function Diet(){
+/* eslint-disable react/jsx-key */
+/* eslint-disable react-hooks/rules-of-hooks */
+import axios from 'axios'
+import { useState } from 'react'
+export default function Diet({diets}:Diets){
     return(
         <div>
-            Diet page
-        </div>
+            <h1>Diet page</h1>
+            {diets.map((diet,index)=>(
+                <ul>
+                    <div key={index}>
+                    <h1>{diet.title}</h1>
+                    <img src={diet.imgurl}/>
+                    <p>{diet.description}</p>
+                    </div>
+                </ul>
+            )
+                
+            )}
+          </div>
     )
 }
-export default Diet
+ 
+interface OneDiet {
+    title: string,
+    imgurl: string,
+    description:string
+}
+interface Diets{
+    diets:OneDiet[]
+}
+
+

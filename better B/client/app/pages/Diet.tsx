77a596676073c09a3d9a16f68cb8c8c3d20e689a
Moez Hamza <hamza.moez@gmail.com>
2022-07-20 12:00:23 +0100

    working on the diet list

--- better B/client/app/pages/Diet.tsx
+++ better B/client/app/pages/Diet.tsx
@@ -1,35 +1,38 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react-hooks/rules-of-hooks */
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-export default function Diet(){
-    const [data,setData]=useState([])
-    interface OneDiet {
-        name: string,
-        img: string,
-        description:string
-    }
-
+// import axios from 'axios'
+// import { useEffect, useState } from 'react'
+import Navbar from './Navbar'
+export default function Diet({diets}:any){
+    // const [data,setData]=useState([])
     
-    useEffect(()=>{
-        fetch("http://localhost:2000/api/diets").then(respone=>respone.json()).then(data=>setData(data))
-    },[])
+    // useEffect(()=>{
+    //     fetch("http://localhost:2000/api/diets").then(respone=>respone.json()).then(data=>setData(data))
+    // },[])
     return(
         <div>
-            {data.map((diet:any,index:number)=>(
+            <Navbar />
                 <ul>
+            {diets.map((diet:any,index:number)=>(
                     <div key={index}>
                     <h1>{diet.name}</h1>
-                    <img src={diet.img} />
+                    <img src={diet.image} />
                     <p>{diet.description}</p>
                     </div>
-                </ul>
             )
-                
             )}
+            </ul>
           </div>
     )
 }
+export async function getStaticProps() {
+    const diets = await fetch ("http://localhost:2000/api/diets").then(response => response.json())
+    return {
+        props:{
+            diets
+        }
+    }
+}   
  
 
 
